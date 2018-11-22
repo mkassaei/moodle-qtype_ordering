bc3bc9b757fe11bca764288655fbb7e8681d6d6a
M Kassaei <m.kassaei@open.ac.uk>
2018-11-22 11:31:29 +0000

    Ordering qtype: Write some PHP unit tests #296740

--- tests/question_test.php
+++ tests/question_test.php
@@ -36,17 +36,43 @@ require_once($CFG->dirroot . '/question/engine/tests/helpers.php');
  */
 class qtype_ordering_question_test extends advanced_testcase {
 
+    /**
+     * return an array of answer codes in the order of given response.
+     * @param object $question the question object
+     * @param array $items array of input items
+     */
+    private function get_response($question, $items) {
+        $md5keys = [];
+        foreach ($items as $item) {
+            foreach ($question->answers as $answer) {
+                if ($item === $answer->answer) {
+                    $md5keys[] = $answer->md5key;
+                    break;
+                }
+            }
+        }
+        return ['response_' . $question->id => implode(',', $md5keys)];
+    }
+
     public function test_grading() {
         /** @var qtype_ordering_question $question */
         $question = test_question_maker::make_question('ordering');
+
+        // Set grading type to 'grading all or nothing'.
+        $question->options->gradingtype = qtype_ordering_question::GRADING_ALL_OR_NOTHING;
         $question->start_attempt(new question_attempt_pending_step(), 1);
 
-        $ids = [];
-        foreach ($question->answers as $answer) {
-            $ids[] = $answer->md5key;
-        }
-
         $this->assertEquals([1, question_state::$gradedright],
-                $question->grade_response(['response_' . $question->id => implode(',', $ids)]));
+                $question->grade_response($this->get_response($question, ['Modular', 'Object', 'Oriented', 'Dynamic', 'Learning', 'Environment'])));
+
+        $this->assertLessThan([1, question_state::$gradedpartial],
+                $question->grade_response($this->get_response($question, ['Oriented', 'Object', 'Dynamic', 'Learning', 'Environment', 'Modular'])));
+
+        $this->assertEquals([0, question_state::$gradedwrong],
+                $question->grade_response($this->get_response($question, ['Environment', 'Learning', 'Dynamic', 'Oriented', 'Object', 'Modular'])));
+
+        $this->assertEquals([0, question_state::$gradedwrong],
+                $question->grade_response($this->get_response($question, ['Oriented', 'Environment', 'Modular', 'Learning', 'Object', 'Dynamic'])));
+
     }
 }
